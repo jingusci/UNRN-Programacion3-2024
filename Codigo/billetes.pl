81e5818ae86638a7cea9ee21fb4a4b7c615c104f
IoachimusRoderici <joaq.rod0@gmail.com>
2024-11-13 21:43:58 -0300

    Mejoras de redacción en prolog.
    Cambié los nombres para que sean predicados y no verbos.
    Hice que evalúe las combinaciones en orden por cantidad de billetes (casi).

--- Codigo/billetes.pl
+++ Codigo/billetes.pl
@@ -1,5 +1,3 @@
-%%writefile billetes.pl
-
 % Base de conocimientos de billetes
 billete(10000).
 billete(2000).
@@ -12,19 +10,37 @@ billete(20).
 billete(10).
 billete(5).
 
+todos_los_billetes(Lista) :-
+    setof(Valor, billete(Valor), ListaAscentente),
+    reverse(ListaAscentente, Lista).
+
 % Regla para calcular la cantidad de billetes a entregar
-calcular_vuelto(Vuelto, Resultado) :-
-    calcular_billetes(Vuelto, [10000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5], Resultado).
+billetes_suman(Total, BilletesUsados) :-
+    todos_los_billetes(BilletesPosibles),
+    billetes_suman_usando(Total, BilletesUsados, BilletesPosibles).
 
-% Regla auxiliar para calcular los billetes
-calcular_billetes(0, _, []). % Caso base: si el vuelto es 0, no se entregan billetes
-calcular_billetes(Vuelto, [Billete|BilletesRestantes], [(Billete, Cantidad)|Resto]) :-
-    Vuelto >= Billete,
-    Cantidad is Vuelto // Billete,
-    NuevoVuelto is Vuelto mod Billete,
-    calcular_billetes(NuevoVuelto, BilletesRestantes, Resto).
-calcular_billetes(Vuelto, [_|BilletesRestantes], Resultado) :-
-    calcular_billetes(Vuelto, BilletesRestantes, Resultado).
+% caso base: si el total es 0 la lista de billetes está vacía.
+billetes_suman_usando(0, [], _).
 
-% Ejemplo de consulta para obtener el vuelto
-% consulta: calcular_vuelto(3870, Resultado).
+% Caso recursivo: Combinaciones que usa el billete más grande.
+billetes_suman_usando(Total, [(BilleteActual, Cantidad)|Resto], [BilleteActual|BilletesRestantes]) :-
+    Total >= BilleteActual,
+    MaxCantidad is Total // BilleteActual, % Calcular cuantos billetes se pueden usar de esta denominación.
+    between_desc(MaxCantidad, 1, Cantidad),  % Prueba diferentes cantidades de billetes, desde el máximo posible hasta 1.
+    MontoRestante is Total - Cantidad * BilleteActual,
+    billetes_suman_usando(MontoRestante, Resto, BilletesRestantes).
+
+% Caso recursivo: Combinaciones que no usan el billete más grande.
+billetes_suman_usando(Total, BilletesUsados, [_|BilletesRestantes]) :-
+    Total > 0,
+    billetes_suman_usando(Total, BilletesUsados, BilletesRestantes).
+
+% Esta regla es igual que between: pasa por todos lo números entre Start y End.
+% La diferencia es que pasa en orden descendiente, lo cual nos sirve para evaluar
+% primero las opciones de vuelto que usan menos billetes.
+between_desc(Start, End, Start) :-
+    Start >= End.
+between_desc(Start, End, X) :-
+    Start > End,
+    Next is Start - 1,
+    between_desc(Next, End, X).
